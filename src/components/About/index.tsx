a50e5109083fe10a0d87fca0267a6ddbbef4d24c
saur0w <sthapliyal085@gmail.com>
2026-08-14 20:20:19 +0530

    feat: add About component and define Landing section styles

---
+++ src/components/About/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function About() {
+    return (
+        <section className={styles.about}>
+
+        </section>
+    );
+}
